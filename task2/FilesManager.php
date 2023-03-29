fb22fb7cae5416f2f7c6eaf8ff428f3505f7501d
Pavel Vladimirovich <sawel@yandex.ru>
2023-03-29 21:40:42 +0800

    task2 finish

--- task2/FilesManager.php
+++ task2/FilesManager.php
@@ -3,28 +3,38 @@
 class FilesManager
 {
     const UPLOAD_DIR = __DIR__ . '/uploads/';
-
     public string $error = '';
     private array $files = [];
 
-    public function __construct(array $files)
+    public function __construct(array $files = [])
     {
         $this->files = $files;
         $this->createUploadDir();
-
     }
 
     private function createUploadDir(): void
     {
-        if (!mkdir(self::UPLOAD_DIR)) {
-            die('Не удалось создать директорию...');
+        if (!is_dir(self::UPLOAD_DIR)) {
+            if (!mkdir(self::UPLOAD_DIR)) {
+                $this->error = 'Не удалось создать директорию...';
+            }
         }
     }
 
     public function saveToUploadDir(): bool
     {
-        if (!$this->checkUploadErrors()) {
+        if (!$this->checkUploadErrors() || $this->error !== '') {
             return false;
+        }
+
+        foreach ($this->files as $file) {
+
+            // тут бы вызвать метод изменения имени, проверки допустимости файла и возможно что-то еще, но будем думать что надо так ...
+
+            if (!move_uploaded_file($file['tmp_name'], self::UPLOAD_DIR . $file['name'])) {
+                $this->error = "Ошибка загрузки «" . $file['name'] . "».\n";
+                return false;
+            }
         }
         return true;
     }
@@ -40,7 +50,20 @@ class FilesManager
                 return false;
             }
         }
+        return true;
+    }
 
+    public function getUploadedFiles(): array
+    {
+        return array_diff(scandir(self::UPLOAD_DIR), array('..', '.'));
+    }
+
+    public function deleteFile(string $filename): bool
+    {
+        if (!unlink(self::UPLOAD_DIR . $filename)) {
+            $this->error = "Ошибка удаления «" . $filename . "».\n";
+            return false;
+        }
         return true;
     }
 }
--- task2/index.php
+++ task2/index.php
@@ -1,17 +1,44 @@
 <?php
 include 'FilesManager.php';
+$fm = new FilesManager($_FILES);
 
 if (isset($_FILES['file'])) {
-    $fm = new FilesManager($_FILES);
     if (!$fm->saveToUploadDir()) {
         echo '<p class="error">' . $fm->error . '</p>';
-        die();
     }
+}
+
+if (isset($_GET['delfile']) && !empty($_GET['delfile'])) {
+    $fm->deleteFile($_GET['delfile']);
+    header('Location: ' . $_SERVER["SCRIPT_NAME"]);
 }
 
 ?>
 
-<form action="" method="post" enctype="multipart/form-data">
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task2</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form action="<?= $_SERVER["SCRIPT_NAME"]; ?>" method="post" enctype="multipart/form-data">
     <input type="file" name="file">
     <input type="submit" value="Отправить">
 </form>
+<hr>
+<?php
+
+foreach ($fm->getUploadedFiles() as $file) {
+    echo '<a title="Удалить" class="file-item" href="' . $_SERVER["SCRIPT_NAME"] . '?delfile=' . $file . '">' . $file . ' </a>';
+}
+
+?>
+</body>
+</html>
+
+
